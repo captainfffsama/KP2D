e59de5d9f2684e7e54fa070e08d0b0b4c1d39059
chiebot_hq <tuanzhangsama@outlook.com>
2021-01-28 16:42:29 +0800

    添加了数据集检查用工具

--- scripts/check_dataset.py
+++ scripts/check_dataset.py
@@ -4,21 +4,21 @@
 # @Author: CaptainHu
 # @Date: 2021-01-28 09:33:41
 # @LastEditors: CaptainHu
+
 import argparse
 import os
-from concurrent import futures
 
 from tqdm import tqdm
 from PIL import Image
+from skimage import io
 
-from kp2d.utils.config import parse_train_file
-from train_keypoint_net_utils import setup_datasets_and_dataloaders
 
 def parse_args():
     parser = argparse.ArgumentParser(description="数据集检查")
     parser.add_argument("--dataset",)
     parser.add_argument("--log")
     args = parser.parse_args()
+    return args
 
 def get_all_file_path(file_dir:str,filter_=('.jpg')) -> list:
     #遍历文件夹下所有的file
@@ -28,7 +28,7 @@ def get_all_file_path(file_dir:str,filter_=('.jpg')) -> list:
 
 def check_img(file_path):
     try:
-        img=Image.open(file_path)
+        img=io.imread(file_path)
     except: 
         img=None
     return img,file_path
@@ -44,12 +44,10 @@ def main(args):
     
     error_list=[]
 
-    with futures.ThreadPoolExecutor(64) as exec:
-        tasks=[exec.submit(check_img,x) for x in all_path]
-        for task in tqdm(futures.as_completed(tasks),total=len(all_path)):
-            img,img_path=task.result()
-            if img is None:
-                error_list.append(img_path+"\n")
+    for img in tqdm(all_path):
+        img,img_path=check_img(img)
+        if img is None:
+            error_list.append(img_path+"\n")
     
     with open(args.log,'w') as fw:
         fw.writelines(error_list)
